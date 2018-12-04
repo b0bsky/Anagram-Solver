3324a4f02f3dad56ab60e9c258c06ee582f5b39f
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2018-12-05 12:25:42 +1300

    Add files via upload

--- solver.py
+++ solver.py
@@ -1,15 +1,20 @@
 # Program to output all possible anagrams that can be created from an inputted string of characters
 
+from itertools import permutations
+
 # Variables for input and output
 users_letters = input("Input letters: ")
 anagrams = []
 
 # Function to check each letter in users input and compares it with word from dictionary, if it is in the given dictionary then add it to anagrams array
 def check_for_anagram( letters, check_word):
-    for letter in letters:
-        if letter in check_word:
-            if not any(letter in s for s in anagrams):
-                anagrams.append(letter)
+    letter_anagrams = []
+    for j in permutations(users_letters):
+        letter_anagrams.append("".join(j))
+    for i in range(0, len(letter_anagrams)):
+        if letter_anagrams[i] == check_word:
+            if not any(letter_anagrams[i] in s for s in anagrams):
+                anagrams.append(letter_anagrams[i])
 
 # Main function
 def main():
